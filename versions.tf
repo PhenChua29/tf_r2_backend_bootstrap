f6b658b0507d795227c18c15e21f9c2a3afcc7dd
Phen Chua <89496400+PhenChua29@users.noreply.github.com>
2026-06-22 23:40:38 +0700

    fix: use local backend

--- versions.tf
+++ versions.tf
@@ -12,20 +12,8 @@ terraform {
     }
   }
 
-  # backend "local" {
-    # path = "state/terraform.state"
-  # }
-
-  backend "s3" {
-    bucket       = "tfstate-cloudflare-r2"
-    key          = "state/terraform.state"
-    use_lockfile = true
-    endpoints = {
-      s3 = "https://3b75aa4988913843d56d261129f9a9c4.r2.cloudflarestorage.com"
-    }
-
-    skip_credentials_validation = true
-    skip_requesting_account_id  = true
+  backend "local" {
+    path = "state/terraform.state"
   }
 }
 
